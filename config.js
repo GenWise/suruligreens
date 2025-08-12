e219a3607ceece6c858458460ba33916ccef7134
Rajesh Panchanathan <rajesh@genwise.in>
2025-08-12 14:39:00 +0530

    Update Google Apps Script URL to new deployment with CORS headers

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ const config = {
     
     // Google Apps Script Web App URL
     // After deploying your Google Apps Script, paste the web app URL here
-    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxDYXTFM29rBrqD80UjjlvjRIw5f72gMlS9honW7BFwwxrwf2na6v793slJQpFnrDMt/exec',
+    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyAczwQsi6s1H67m6TEa8_nRyNha2FkFnXbyvkeaaluOPTdP5zztSymEQcnyKCtNmtcZQ/exec',
     
     // Delivery Configuration
     DELIVERY_DAYS: ['Tuesday', 'Friday'],
